cd71814d834f241e45fbd0d09043bbe75b1dbff0
guialves987 <gui.santana9876@gmail.com>
2025-08-31 20:34:55 +0000

    desafio nivel aventureiro: adicionei variável do cavalo, implementei a estrutura de seu movimento usando loops aninhados

--- xadrez.c
+++ xadrez.c
@@ -3,7 +3,7 @@
 int main() {
 
     //variáveis das peças
-    int torre = 0, bispo = 0, rainha = 0;
+    int torre = 0, bispo = 0, rainha = 0, cavalo = 0;
 
     // Indicação de que a torre está se movimentando
     printf("*** Torre ***\n");
@@ -14,15 +14,19 @@ int main() {
         printf("Direita\n");
         torre++;
     }
+
+    printf("\n");
     
     // Indicação de que o bispo está se movimentando
     printf("*** Bispo ***\n");
 
     // Estrutura de movimentação do bispo
     do {
-        printf("diagonal cima/direita\n");
+        printf("Diagonal cima/direita\n");
         bispo++;
     } while (bispo < 5);
+
+    printf("\n");
 
     // Indicação de que a rainha está se movimentando
     printf("*** Rainha ***\n");
@@ -31,6 +35,24 @@ int main() {
     for (rainha; rainha < 8; rainha++) {
         printf("Esquerda\n");
     }
+
+    printf("\n");
+
+    // Indicação da movimentação do cavalo
+    printf("*** Cavalo ***\n");
+
+    // Estrutura de movimentação do cavalo com loop aninhado
+    while (cavalo < 1)
+    {
+        for (cavalo; cavalo < 2; cavalo++)
+        {
+            printf("Baixo\n");
+        }
+        printf("Esquerda\n");
+        cavalo++;
+        printf("\n");
+    }
+    
 
     return 0;
 }
